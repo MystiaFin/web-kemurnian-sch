d0f37fc88a0d0210ef0cc81156a714b02d24f590
MystiaFin <cytrsx01@gmail.com>
2025-08-29 07:29:04 +0700

    finished get route for kurikulum

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HeroBannerController;
+use App\Http\Controllers\KurikulumController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +13,6 @@ Route::get('/user', function (Request $request) {
 Route::get('/hero-banners', [HeroBannerController::class, 'index'])->name('hero-banners.index');
 Route::post('/hero-banners', [HeroBannerController::class, 'store'])->name('hero-banners.store');
 Route::delete('/hero-banners/{id}', [HeroBannerController::class, 'destroy'])->name('hero-banners.destroy');
+
+// Kurikulum routes
+Route::get('/kurikulum', [KurikulumController::class, 'index'])->name('kurikulum.index');
